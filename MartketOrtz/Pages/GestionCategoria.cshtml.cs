71fb8590ef7559d52031725322d43700367232a3
Joaquin <bevodroid3@gmail.com>
2026-05-21 21:48:32 -0400

    Paginas para editar

---
+++ MartketOrtz/Pages/GestionCategoria.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MartketOrtz.Pages
+{
+    public class GestionCategoriaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
